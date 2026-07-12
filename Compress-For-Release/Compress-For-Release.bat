a03d400a0579c2d8fa44c50578ab0822ee8c4af1
RuslOFFICIAL <ryslan14523uaiyt@gmail.com>
2026-07-12 21:19:47 +0300

    Add Compress-For-Release.

---
+++ Compress-For-Release/Compress-For-Release.bat
@@ -0,0 +1,46 @@
+@echo off
+cd /d "%~dp0"
+setlocal
+
+REM .conf files.
+if exist "..\.conf-files\Variables.conf" (
+	for /f "usebackq eol=# tokens=1,2 delims==" %%A in ("..\.conf-files\Variables.conf") do set "%%A=%%~B"
+)
+
+echo Compress-For-Release %DUU_Version%&echo.
+goto CompressingProc
+
+REM Compressing process.
+:CompressingProc
+REM Define paths relative to the script location.
+set "SourceDir=%~dp0.."
+set "StagingDir=%~dp0..\TempRelease"
+set "ZipFolder=%~dp0..\Releases"
+set "ZipFile=%ZipFolder%\DUU_%DUU_Version%.zip"
+
+echo Cleaning release folder...
+for %%f in ("%ZipFolder%\DUU_*.zip") do (
+	echo Removing old ZIP: "%%~nxf"
+	del "%%f" /f /q
+)
+
+echo Preparing release folder (excluding all .conf files)...
+robocopy "%SourceDir%" "%StagingDir%" /E /XF *.lnk /XD TempRelease Releases .git
+
+echo.&echo Compressing into .zip file...
+REM Create the output directory if it doesn't exist.
+if not exist "%ZipFolder%" mkdir "%ZipFolder%"
+
+REM Use PowerShell to compress the staging contents.
+powershell -Command "Compress-Archive -Path '%StagingDir%\*' -DestinationPath '%ZipFile%' -Force"
+
+echo.
+echo Cleaning up temporary folders...
+rmdir /s /q "%StagingDir%"
+goto End
+
+REM End.
+:End
+endlocal
+echo.&echo Done!&echo Your release is ready inside the "Releases" folder.
+pause
